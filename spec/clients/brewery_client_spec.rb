4d3346b59c1b68c9f9719db9f74745f7d7f05636
Mike Heft <mikeheft@gmail.com>
2020-03-21 17:27:54 +0000

    Add spec that show by_city param returns results related to that city only

--- spec/clients/brewery_client_spec.rb
+++ spec/clients/brewery_client_spec.rb
@@ -6,15 +6,15 @@ RSpec.describe 'Brewery DB Client' do
   it 'is a Brewery object' do
     expect(subject).to be_a(Brewery)
   end
-
+  
   it '#list' do
     use_cassette('brew_list') do
-      @breweries = subject.list
+      @breweries                   = subject.list
       @first_page_first_brewery_id = @breweries.first[:id]
     end
     
     use_cassette('second_page_breweries') do
-      @breweries_2 = subject.list(page: 2)
+      @breweries_2                  = subject.list(page: 2)
       @second_page_first_brewery_id = @breweries_2.first[:id]
     end
     
@@ -23,21 +23,32 @@ RSpec.describe 'Brewery DB Client' do
     expect(@first_page_first_brewery_id).not_to eq(@second_page_first_brewery_id)
   end
 
+  it 'should #list with params' do
+    use_cassette('list_with_params') do
+      breweries = subject.list(by_city: 'Denver')
+      
+      expect(breweries).not_to be_empty
+      expect(breweries.all? { |h| h[:city] = 'Denver' }).to be_truthy
+    end
+  end
+  
   it '#find' do
     use_cassette('find_brewery_2') do
       brewery = subject.find(id: 2)
-
+      
       expect(brewery[:id]).to eq(2)
       expect(brewery[:name]).to eq('Avondale Brewing Co')
       expect(brewery[:name]).not_to eq('Comrade Brewing Co')
     end
   end
-
+  
   it '#search' do
     use_cassette('search_breweries') do
-      brewery = subject.search(query: 'Comrade')
-      require 'pry'; binding.pry
+      breweries = subject.search(query: 'Comrade')
+      
+      expect(breweries).not_to be_empty
+      expect(breweries.any? { |h| h[:name] =~ /Comrade/ }).to be_truthy
     end
   end
-  
+
 end
