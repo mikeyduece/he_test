1b6f6074cb3b07f9481e907ec66643a50b6f8aef
Mike Heft <mikeheft@gmail.com>
2020-03-21 11:44:29 +0000

    Add uniqueness validation for email and also unique index in user email

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   include Authentication::Doorkeeper
   
   validates :first_name, :last_name, :email, :password, presence: true
+  validates :email, uniqueness: true
 end
