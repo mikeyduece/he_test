5a254df535db140ab3cabb96bfa26ef0044cbe0f
Mike Heft <mikeheft@gmail.com>
2020-03-21 18:56:29 +0000

    Add expectation that service also creates Search object

--- spec/services/brewery_search_service_spec.rb
+++ spec/services/brewery_search_service_spec.rb
@@ -3,15 +3,15 @@ require 'rails_helper'
 RSpec.describe 'BrewerySearchService' do
   it 'should create a Search for new queries' do
     use_cassette('list_with_params') do
-      BrewerySearchService::Create.call(query: { by_city: 'Denver' }) do |success, failure|
-        expect(success).not_to be_nil
-        expect(success.resource).to be_a(Search)
-        expect(success.all? { |h| h[:city] = 'Denver' }).to be_truthy
-        
-        expect(failure).to be_nil
+      BrewerySearchService::Create.call({ by_city: 'Denver' }) do |success, _failure|
+        success.call do |resource|
+          expect(resource).to be_a(Array)
+          expect(resource.first).to be_a(Hash)
+          expect(resource.all? { |h| h[:city].eql?('Denver') })
+        end
       end
       
-      search = Search.find_by('query @> ?', { city: 'Denver' })
+      search = Search.find_by('query @> ?', { by_city: 'Denver' }.to_json)
       
       expect(search).not_to be_nil
     end
