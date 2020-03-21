6087e5fe341258ec7b40d977ed7e6b3037866569
Mike Heft <mikeheft@gmail.com>
2020-03-21 15:59:42 +0000

    Add Search model with custom validation ensuring queries are unique

---
+++ app/models/search.rb
@@ -0,0 +1,11 @@
+class Search < ApplicationRecord
+  store_accessor :query, :types, :city, :state, :tags, :name
+  
+  validate :ensure_unique_query, on: :create
+  
+  private
+  
+  def ensure_unique_query
+    errors[:base] << 'Search with these params already exists' if self.class.exists?(query: self.query)
+  end
+end
