f1abeb405ab41290acb51919058d357d9d65b58c
Mike Heft <mikeheft@gmail.com>
2020-03-21 12:01:57 +0000

    Add User#name

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   
   validates :first_name, :last_name, :email, :password, presence: true
   validates :email, uniqueness: true
+  
+  def name
+    "#{first_name} #{last_name}"
+  end
 end
