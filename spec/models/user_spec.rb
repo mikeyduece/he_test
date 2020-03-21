60847bd702b4023f549edb4c474c41f4878b422a
Mike Heft <mikeheft@gmail.com>
2020-03-21 11:24:49 +0000

    Add association and validation specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context :associations do
+    it { should have_many(:access_grants) }
+    it { should have_many(:access_tokens) }
+  end
+  
+  context :validations do
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:password) }
+    it { should validate_presence_of(:first_name) }
+    it { should validate_presence_of(:last_name) }
+  end
 end
