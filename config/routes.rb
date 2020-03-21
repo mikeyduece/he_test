f48beeee321e0af6aaad87a542d6058831191df4
Mike Heft <mikeheft@gmail.com>
2020-03-21 11:36:17 +0000

    Add route/controller for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   use_doorkeeper
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    namespace :v1 do
+      resources :users, module: :users, only: %i[create]
+      
+    end
+  end
 end
