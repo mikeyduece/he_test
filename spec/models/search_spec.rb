75c6f47b6490095960cb5e79e47d0a3aacefd41a
Mike Heft <mikeheft@gmail.com>
2020-03-21 16:17:24 +0000

    Add expectation to check error message returned for invalid search

--- spec/models/search_spec.rb
+++ spec/models/search_spec.rb
@@ -3,20 +3,22 @@ require 'rails_helper'
 RSpec.describe Search, type: :model do
   context :validations do
     it 'should validate uniqueness' do
-      search_1 = Search.create(query: { city: 'Denver', tags: ['patio', 'food-truck'] })
-      search_2 = Search.new(query: { city: 'Denver', tags: ['patio', 'food-truck'] })
+      search_1      = Search.create(query: { city: 'Denver', tags: ['patio', 'food-truck'] })
+      search_2      = Search.new(query: { city: 'Denver', tags: ['patio', 'food-truck'] })
+      error_message = search_2.errors.full_messages.first
       
       expect(search_1).to be_valid
       expect(search_2).not_to be_valid
+      expect(error_message).to eq('Search with these params already exists')
     end
-
+    
     it 'should allow for different queries' do
       search_1 = Search.new(query: { city: 'Denver', tags: ['patio', 'food-truck'] })
       search_2 = Search.new(query: { city: 'Boulder', tags: ['patio', 'food-truck'] })
-  
+      
       expect(search_1).to be_valid
       expect(search_2).to be_valid
     end
   end
-  
+
 end
