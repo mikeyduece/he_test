7a41d5d9ee18b39055dd500ff6b161fa9c8c8a1b
Mike Heft <mikeheft@gmail.com>
2020-03-21 18:27:48 +0000

    Refactor brwery search spec

--- spec/services/brewery_search_service_spec.rb
+++ spec/services/brewery_search_service_spec.rb
@@ -1,18 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe 'BrewerySearchService' do
-  subject { BrewerySearchService::Create.new({by_city: 'Denver'}) }
-  
-  it 'should return a service object' do
-    expect(subject).to be_a(BrewerySearchService::Create)
-  end
-  
   it 'should create a Search for new queries' do
     use_cassette('list_with_params') do
-      search           = Search.find_by('query @> ?', { city: 'Denver' })
+      BrewerySearchService::Create.call(query: { by_city: 'Denver' }) do |success, failure|
+        expect(success).not_to be_nil
+        expect(success.resource).to be_a(Search)
+        expect(success.all? { |h| h[:city] = 'Denver' }).to be_truthy
+        
+        expect(failure).to be_nil
+      end
       
-      expect(subject).not_to be_empty
-      expect(subject.all? { |h| h[:city] = 'Denver' }).to be_truthy
+      search = Search.find_by('query @> ?', { city: 'Denver' })
+      
       expect(search).not_to be_nil
     end
   end
