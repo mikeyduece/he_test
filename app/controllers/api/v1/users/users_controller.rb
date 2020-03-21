2d9e840a3b40aa01305baa9df874df127af94c0b
Mike Heft <mikeheft@gmail.com>
2020-03-21 11:45:03 +0000

    Add create action to UsersController

--- app/controllers/api/v1/users/users_controller.rb
+++ app/controllers/api/v1/users/users_controller.rb
@@ -1,3 +1,19 @@
 class Api::V1::Users::UsersController < ApplicationController
+  skip_before_action :doorkeeper_authorize!, only: :create
   
+  def create
+    user = User.create(user_params)
+    if user
+      success_response(201, user: serialized_resource(user, ::Users::UserBlueprint, view: :extended))
+    else
+      error_response('A user with that email already exists', 404)
+    end
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password)
+  end
+
 end
