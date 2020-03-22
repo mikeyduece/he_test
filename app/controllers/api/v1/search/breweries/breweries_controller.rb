ed37e3c6355772188ec408df887b5a897313b91e
Mike Heft <mikeheft@gmail.com>
2020-03-22 06:31:18 +0000

    Add route/controller for breweries

---
+++ app/controllers/api/v1/search/breweries/breweries_controller.rb
@@ -0,0 +1,6 @@
+class Api::V1::Search::Breweries::BreweriesController < ApplicationController
+
+  def index
+    render json: 'Hello'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users, module: :users, only: %i[create]
       
+      scope module: :search do
+        resources :breweries, module: :breweries, only: %i[index show]
+      end
     end
   end
 end
