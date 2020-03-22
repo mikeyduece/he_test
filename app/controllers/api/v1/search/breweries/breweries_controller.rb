07c30dff9826509987434a14e568a1a1989290a3
Mike Heft <mikeheft@gmail.com>
2020-03-22 07:08:07 +0000

    Add call to brewery service to list breweries

--- app/controllers/api/v1/search/breweries/breweries_controller.rb
+++ app/controllers/api/v1/search/breweries/breweries_controller.rb
@@ -1,6 +1,13 @@
 class Api::V1::Search::Breweries::BreweriesController < ApplicationController
 
   def index
-    render json: 'Hello'
+    BrewerySearchService::Index.call do |success, failure|
+      success.call do |resource|
+        success_response(200, breweries: serialized_response(resource, Breweries::BreweryBlueprint, view: :extended))
+      end
+      
+      failure.call { |error| error_response(404, error) }
+    end
   end
+  
 end
