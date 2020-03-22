fa7d8ca497cd92e5e185d0c6db65dcd37916ae72
Mike Heft <mikeheft@gmail.com>
2020-03-22 07:26:35 +0000

    Add spec to show params work with service

--- spec/requests/api/v1/search/breweries/breweries_list_request_spec.rb
+++ spec/requests/api/v1/search/breweries/breweries_list_request_spec.rb
@@ -25,6 +25,22 @@ RSpec.describe 'GET /breweries', type: :request do
         expect(breweries.first).to have_key(:brewery_type)
       end
     end
+    
+    it 'should list breweries by city' do
+      use_cassette('list_with_params') do
+        get api_v1_breweries_path, params: { by_city: 'Denver' }, headers: { 'Authorization': authorization }
+        json      = parse_json(response.body)
+        breweries = json[:breweries]
+        
+        expect(response).to be_successful
+        expect(response.status).to eq(200)
+        expect(breweries).to be_a(Array)
+        expect(breweries).not_to be_empty
+        expect(breweries.first).to have_key(:name)
+        expect(breweries.first).to have_key(:brewery_type)
+        expect(breweries.all? { |h| h[:city].eql?('Denver') })
+      end
+    end
   
   end
 end
