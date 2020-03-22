2b002d24e4e49195679e40d21ba6345bfcb9bb9e
Mike Heft <mikeheft@gmail.com>
2020-03-22 10:20:45 +0000

    Refactor search service spec to account for different service name

--- spec/services/brewery_search_service_spec.rb
+++ spec/services/brewery_search_service_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'BrewerySearchService' do
   it 'should create a Search for new queries' do
     use_cassette('list_with_params') do
-      brewery_service(:create, {by_city: 'Denver'}) do |success, _failure|
+      brewery_service(:index, { by_city: 'Denver' }) do |success, _failure|
         success.call do |resource|
           expect(resource).to be_a(Array)
           expect(resource.first).to be_a(Hash)
@@ -21,7 +21,7 @@ RSpec.describe 'BrewerySearchService' do
   it 'should reuse queries that match in the DB' do
     2.times do
       use_cassette('list_with_params') do
-        brewery_service(:create, { by_city: 'Denver' }) do |success, _failure|
+        brewery_service(:index, { by_city: 'Denver' }) do |success, _failure|
           success.call { |resource| expect(resource).to be_a(Array) }
         end
       end
@@ -32,17 +32,28 @@ RSpec.describe 'BrewerySearchService' do
   
   it 'should create new search for new query' do
     use_cassette('list_with_params') do
-      brewery_service(:create, { by_city: 'Denver' }) do |success, _failure|
+      brewery_service(:index, { by_city: 'Denver' }) do |success, _failure|
         success.call { |resource| expect(resource).to be_a(Array) }
       end
     end
     
     use_cassette('list_with_params') do
-      brewery_service(:create, { by_city: 'Boston' }) do |success, _failure|
+      brewery_service(:index, { by_city: 'Boston' }) do |success, _failure|
         success.call { |resource| expect(resource).to be_a(Array) }
       end
     end
     
     expect(Search.count).to eq(2)
+  end
+  
+  it 'should raise an error for invalid params' do
+    brewery_service(:index, { by_stuff: 'Testing' }) do |success, failure|
+      success.call { |resource| expect(resource).to be_a(NoTrigger) }
+      failure.call do |resource|
+        expect(resource).to be_a(Search::InvalidQueryParams)
+        expect(resource.status).to eq(404)
+        expect(resource.message).to eq('Please only use the following query params: [:by_type, :by_city, :by_state, :by_tags, :by_name, :page, :per_page, :id]')
+      end
+    end
   end
 end
