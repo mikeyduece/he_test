cd6436e4f3eb72f51366d9fbd262b5cfad11c31d
Mike Heft <mikeheft@gmail.com>
2020-03-22 08:07:12 +0000

    Add show action to breweries controller

--- app/controllers/api/v1/search/breweries/breweries_controller.rb
+++ app/controllers/api/v1/search/breweries/breweries_controller.rb
@@ -10,4 +10,14 @@ class Api::V1::Search::Breweries::BreweriesController < ApplicationController
     end
   end
   
+  def show
+    BrewerySearchService::Show.call(params, :find) do |success, failure|
+      success.call do |resource|
+        success_response(200, brewery: serialized_response(resource, Breweries::BreweryBlueprint, view: :extended))
+      end
+    
+      failure.call { |error| error_response(404, error) }
+    end
+  end
+  
 end
